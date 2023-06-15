73a4b7ca014ac23b67eb1310ca1e5609f1284fb0
Nikita Kovalev <65085140+bahost@users.noreply.github.com>
2023-06-15 16:16:01 +0200

    added current date filter and removed drop table clause

--- migrations/sql/mart.f_customer_retention.sql
+++ migrations/sql/mart.f_customer_retention.sql
@@ -1,5 +1,4 @@
-drop table if exists mart.f_customer_retention;
-create table mart.f_customer_retention(
+create table if not exists mart.f_customer_retention(
     new_customers_count int not null,
     returning_customers_count int not null,
     refunded_customer_count int not null,
@@ -8,19 +7,20 @@ create table mart.f_customer_retention(
     item_id int,
     new_customers_revenue float,
     returning_customers_revenue float,
-    customers_refunded int
+    customers_refunded int,
+    primary key (period_id,item_id)
 );
 
-insert into mart.f_customer_retention (period_name,
-                                        period_id,
-                                        item_id,
-					new_customers_count,
+insert into mart.f_customer_retention (new_customers_count,
                                         returning_customers_count,
                                         refunded_customer_count,
+                                        period_name,
+                                        period_id,
+                                        item_id,
                                         new_customers_revenue,
                                         returning_customers_revenue,
                                         customers_refunded)
-with t as(
+with(
     select 
         s.customer_id as customer_id,
         c.week_of_year as week_of_year,
@@ -28,21 +28,23 @@ with t as(
         s.payment_amount as payment_amount,
         s.item_id as item_id,
         ---
-        count(s.id) over(partition by c.week_of_year, s.customer_id) as qty_cnt
+        count(s.id) over(partition by t.week_of_year, t.customer_id) as qty_cnt,
     from mart.f_sales as s
-    left join mart.d_calendar as c
+    join mart.d_calendar as c
         on s.date_id = c.date_id
-),
+    where c.date_actual = "{{ ds }}"
+) as t,
 
-t2 as(
+with(
     select distinct
         s.customer_id as customer_id,
-        c.week_of_year as week_of_year
+        c.week_of_year as week_of_year,
     from mart.f_sales as s
-    left join mart.d_calendar as c
+    join mart.d_calendar as c
         on s.date_id = c.date_id
-    where s.payment_amount < 0
-)
+    where payment_amount < 0
+        and c.date_actual = "{{ ds }}"
+) as t2,
 
 select
     'weekly' as period_name,
@@ -79,10 +81,22 @@ left join t2
 on t.customer_id = t2.customer_id
     and t.week_of_year = t2.week_of_year
 group by 
-    t.week_of_year,
+    t.week_of_year
     t.item_id
 ;
 
-
-
-
+/*
+mart.f_customer_retention
+1. new_customers_count — кол-во новых клиентов (тех, которые сделали только один 
+заказ за рассматриваемый промежуток времени).
+2. returning_customers_count — кол-во вернувшихся клиентов (тех,
+которые сделали только несколько заказов за рассматриваемый промежуток времени).
+3. refunded_customer_count — кол-во клиентов, оформивших возврат за 
+рассматриваемый промежуток времени.
+4. period_name — weekly.
+5. period_id — идентификатор периода (номер недели или номер месяца).
+6. item_id — идентификатор категории товара.
+7. new_customers_revenue — доход с новых клиентов.
+8. returning_customers_revenue — доход с вернувшихся клиентов.
+9. customers_refunded — количество возвратов клиентов. 
+*/
