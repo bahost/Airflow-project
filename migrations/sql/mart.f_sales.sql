e6a49b954d121f8066445995fefc7744524710f8
Nikita Kovalev <65085140+bahost@users.noreply.github.com>
2023-06-15 16:14:54 +0200

    Update mart.f_sales.sql

--- migrations/sql/mart.f_sales.sql
+++ migrations/sql/mart.f_sales.sql
@@ -1,3 +1,8 @@
+-- Удаляем из факта данные за текущий день
+delete * 
+from mart.f_sales 
+where date_id in (select date_id from mart.d_calendar where date_actual = '{{ds}}');
+
 insert into mart.f_sales (date_id, item_id, customer_id, city_id, status, quantity, payment_amount)
 select 
     dc.date_id, 
@@ -6,7 +11,10 @@ select
     city_id, 
     'shipped' as status,
     quantity, 
-    if(uol.status = 'refunded', -1 * payment_amount, payment_amount) as payment_amount
+    case
+        when uol.status = 'refunded' then -1 * payment_amount
+        else payment_amount
+    end as payment_amount
 from staging.user_order_log uol
 left join mart.d_calendar as dc 
     on uol.date_time::Date = dc.date_actual
