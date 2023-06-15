511d77a107707f76ff978fbf15eed7ee413acdeb
Nikita Kovalev <65085140+bahost@users.noreply.github.com>
2023-06-15 17:24:04 +0200

    Update mart.f_customer_retention.sql

--- migrations/sql/mart.f_customer_retention.sql
+++ migrations/sql/mart.f_customer_retention.sql
@@ -11,16 +11,19 @@ create table if not exists mart.f_customer_retention(
     primary key (period_id,item_id)
 );
 
-insert into mart.f_customer_retention (new_customers_count,
-                                        returning_customers_count,
-                                        refunded_customer_count,
-                                        period_name,
+delete from mart.f_customer_retention
+where period_id in (select week_of_year from mart.d_calendar where date_actual = '{{ds}}');
+
+insert into mart.f_customer_retention (period_name,
                                         period_id,
                                         item_id,
+                                        new_customers_count,
+                                        returning_customers_count,
+                                        refunded_customer_count,
                                         new_customers_revenue,
                                         returning_customers_revenue,
                                         customers_refunded)
-with(
+with t as(
     select 
         s.customer_id as customer_id,
         c.week_of_year as week_of_year,
@@ -28,23 +31,23 @@ with(
         s.payment_amount as payment_amount,
         s.item_id as item_id,
         ---
-        count(s.id) over(partition by t.week_of_year, t.customer_id) as qty_cnt,
+        count(s.id) over(partition by c.week_of_year, s.customer_id) as qty_cnt
     from mart.f_sales as s
     join mart.d_calendar as c
         on s.date_id = c.date_id
-    where c.date_actual = "{{ ds }}"
-) as t,
+    where c.date_actual = '{{ ds }}'
+),
 
-with(
+t2 as(
     select distinct
         s.customer_id as customer_id,
-        c.week_of_year as week_of_year,
+        c.week_of_year as week_of_year
     from mart.f_sales as s
     join mart.d_calendar as c
         on s.date_id = c.date_id
     where payment_amount < 0
-        and c.date_actual = "{{ ds }}"
-) as t2,
+        and c.date_actual = '{{ ds }}'
+)
 
 select
     'weekly' as period_name,
@@ -81,7 +84,7 @@ left join t2
 on t.customer_id = t2.customer_id
     and t.week_of_year = t2.week_of_year
 group by 
-    t.week_of_year
+    t.week_of_year,
     t.item_id
 ;
 
